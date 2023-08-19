ae75fd095bd222cd3c8fc7da708a1e369b3b4816
Rustam Musin <jvmusin@gmail.com>
2023-08-19 21:57:33 +0100

    Working time measurer

--- kotlin-ir-plugin/src/test/kotlin/com/bnorm/template/IrPluginTest.kt
+++ kotlin-ir-plugin/src/test/kotlin/com/bnorm/template/IrPluginTest.kt
@@ -20,32 +20,43 @@ package com.bnorm.template
 
 import com.tschuchort.compiletesting.KotlinCompilation
 import com.tschuchort.compiletesting.SourceFile
-import kotlin.test.assertEquals
 import org.jetbrains.kotlin.compiler.plugin.ComponentRegistrar
 import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class IrPluginTest {
   @Test
   fun `IR plugin success`() {
     val result = compile(
-      sourceFile = SourceFile.kotlin(
-        "main.kt", """
+        sourceFile = SourceFile.kotlin(
+            "main.kt", """
+annotation class DebugLog
+
 fun main() {
-  println(debug())
+    println(greet())
+    println(greet(name = "Kotlin IR"))
 }
 
-fun debug() = "Hello, World!"
-"""
-      )
+@DebugLog
+fun greet(greeting: String = "Hello", name: String = "World"): String {
+    Thread.sleep(15)
+    return "${'$'}greeting, ${'$'}name!"
+}
+""".trimIndent()
+        )
     )
     assertEquals(KotlinCompilation.ExitCode.OK, result.exitCode)
+
+    val kClazz = result.classLoader.loadClass("MainKt")
+    val main = kClazz.declaredMethods.single { it.name == "main" && it.parameterCount == 0 }
+    main.invoke(null)
   }
 }
 
 fun compile(
-  sourceFiles: List<SourceFile>,
-  plugin: ComponentRegistrar = TemplateComponentRegistrar(),
+    sourceFiles: List<SourceFile>,
+    plugin: ComponentRegistrar = TemplateComponentRegistrar(),
 ): KotlinCompilation.Result {
   return KotlinCompilation().apply {
     sources = sourceFiles
@@ -56,8 +67,8 @@ fun compile(
 }
 
 fun compile(
-  sourceFile: SourceFile,
-  plugin: ComponentRegistrar = TemplateComponentRegistrar(),
+    sourceFile: SourceFile,
+    plugin: ComponentRegistrar = TemplateComponentRegistrar(),
 ): KotlinCompilation.Result {
   return compile(listOf(sourceFile), plugin)
 }
