7fd4b26bcf02dc1b5cb7e47f5e06a078e8dcd30f
Rustam Musin <jvmusin@gmail.com>
2023-08-23 01:37:42 +0100

    Add annotation support

--- kotlin-ir-plugin/src/main/kotlin/com/bnorm/template/TemplateComponentRegistrar.kt
+++ kotlin-ir-plugin/src/main/kotlin/com/bnorm/template/TemplateComponentRegistrar.kt
@@ -26,28 +26,32 @@ import org.jetbrains.kotlin.com.intellij.mock.MockProject
 import org.jetbrains.kotlin.compiler.plugin.ComponentRegistrar
 import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
 import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.name.FqName
 
 @AutoService(ComponentRegistrar::class)
 class TemplateComponentRegistrar(
-  private val defaultString: String,
-  private val defaultFile: String,
+        private val defaultString: String,
+        private val defaultFile: String,
+        private val defaultAnnotation: String,
 ) : ComponentRegistrar {
 
-  @Suppress("unused") // Used by service loader
-  constructor() : this(
-    defaultString = "Hello, World!",
-    defaultFile = "file.txt"
-  )
+    @Suppress("unused") // Used by service loader
+    constructor() : this(
+            defaultString = "Hello, World!",
+            defaultFile = "file.txt",
+            defaultAnnotation = "InlineThisClass",
+    )
 
-  override fun registerProjectComponents(
-    project: MockProject,
-    configuration: CompilerConfiguration
-  ) {
-    val messageCollector = configuration.get(CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY, MessageCollector.NONE)
-    val string = configuration.get(TemplateCommandLineProcessor.ARG_STRING, defaultString)
-    val file = configuration.get(TemplateCommandLineProcessor.ARG_FILE, defaultFile)
+    override fun registerProjectComponents(
+            project: MockProject,
+            configuration: CompilerConfiguration
+    ) {
+        val messageCollector = configuration.get(CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY, MessageCollector.NONE)
+        val string = configuration.get(TemplateCommandLineProcessor.ARG_STRING, defaultString)
+        val file = configuration.get(TemplateCommandLineProcessor.ARG_FILE, defaultFile)
+        val annotation = configuration.get(TemplateCommandLineProcessor.ARG_ANNOTATION, defaultAnnotation)
 
 //    IrGenerationExtension.registerExtension(project, TemplateIrGenerationExtension(messageCollector, string, file))
-    IrGenerationExtension.registerExtension(project, MyIrGenerationExtension(messageCollector))
-  }
+        IrGenerationExtension.registerExtension(project, MyIrGenerationExtension(messageCollector, FqName(annotation)))
+    }
 }
