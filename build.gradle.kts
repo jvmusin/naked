c69e585d3d89eb03ff9301cedf10956a0a117bee
Rustam Musin <jvmusin@gmail.com>
2023-08-26 03:14:18 +0100

    Preparing for publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ buildscript {
 }
 
 plugins {
-    kotlin("jvm") version "1.9.0" apply false
+    kotlin("jvm") version "1.9.10" apply false
     id("org.jetbrains.dokka") version "1.8.20" apply false
     id("com.gradle.plugin-publish") version "1.2.1" apply false
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
--- see-through-annotation/build.gradle.kts
+++ see-through-annotation/build.gradle.kts
@@ -2,10 +2,36 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm")
+    id("maven-publish")
+    id("signing")
 }
 
 java.toolchain.languageVersion = JavaLanguageVersion.of(11)
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "11"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("seeThroughAnnotation") {
+            from(components["kotlin"])
+            pom {
+                name = "SeeThrough Kotlin Compiler Plugin Annotation"
+                description = "Annotation for the SeeThrough Kotlin Compiler Plugin " +
+                        "which is used for marking value classes to inline."
+                url = "https://github.com/jvmusin/see-through"
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["seeThroughAnnotation"])
 }
